82b4e7e54039b95a53bd91fe39070b48a386383d
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-15 02:11:54 +0600

    added services.json file and fetch file in the home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Services from '../Services/Services';
 import './Home.css'
+import repaire1 from '../../../Images/repaire1.png';
+
+const services = [
+    { id: 1, name: 'oil change', price: 100, description: '', img: 'https://i.ibb.co/vhqZbWh/repaire1.png' },
+]
+
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Services></Services>
         </div>
     );
 };
