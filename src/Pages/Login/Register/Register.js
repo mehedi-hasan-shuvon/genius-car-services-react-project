9ea6c4c9dd2720456d73a53e46fed0e3bf6c8f83
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-16 19:44:00 +0600

    added register page and make the form

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css'
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+
+    const handelRegister=event=>{
+        event.preventDefault();
+    }
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='Register-form'>
+            <h2 className='text-center text-primary mt-4'>Please Register</h2>
+            <form onSubmit={handelRegister}>
+                <input type="text" name="name" id="" placeholder='Your name' />
+                <input type="email" name="email" id="" placeholder='Your email Address' required />
+                <input type="password" name="password" id="" placeholder='your password' required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>All ready have an Account? <Link to='/login' onClick={navigateLogin} className='text-danger text-decoration-none pe-auto'>Please Login</Link></p>
         </div>
     );
 };
