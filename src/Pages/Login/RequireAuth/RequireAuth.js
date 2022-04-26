c07ede98212a844da6357f8f1c1ad84297d7c621
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-27 01:29:19 +0600

    added email verification requrement

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({ children }) => {
+
     const [user, loading] = useAuthState(auth);
+
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
     if (loading) {
         return <Loading></Loading>
@@ -16,6 +20,23 @@ const RequireAuth = ({ children }) => {
         // along to that page after they login, which is a nicer user experience
         // than dropping them off on the home page.
         return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    if (!user.emailVerified) {
+        return <div className='text-center'>
+            <h2 className='text-center text-danger'>Your Email is not Verified!!</h2>
+            <h2 className='text-center text-success'>Please verify your email address!!</h2>
+            <button
+                className='btn btn-primary'
+                onClick={async () => {
+                    await sendEmailVerification();
+                    toast('Sent email');
+                }}
+            >
+                send Verification email again
+            </button>
+            <ToastContainer />
+        </div>
     }
 
     return children;
